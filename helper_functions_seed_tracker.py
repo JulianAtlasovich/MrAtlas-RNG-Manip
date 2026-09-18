64af2f8c79adb10a60024d16830b6bd6b9a3e90d
JulianAtlas <julianatlasovich@gmail.com>
2026-09-18 17:37:25 -0300

    changes to the fusion_calculator_combinations

--- helper_functions_seed_tracker.py
+++ helper_functions_seed_tracker.py
@@ -340,25 +340,24 @@ def can_card_be_equipped(card1,card2):
 def get_strongest_fusions_from_hand(hand, result_count=3):
   results = []
 
-  for fusion_combination in Constants.fusion_combinations:
+  for fusion_combination in Constants.fusion_calculator_combinations:
     if len(fusion_combination) < 2 or max(fusion_combination) >= len(hand):
       continue
 
     card_to_play = copy.deepcopy(hand[fusion_combination[0]])
-    has_valid_combination = False
+    has_failed_step = False
     for card_index in fusion_combination[1:]:
       next_card = copy.deepcopy(hand[card_index])
       fusion_result, fusion_succeeded = find_fusion(card_to_play, next_card)
       if fusion_succeeded:
         card_to_play = fusion_result
-        has_valid_combination = True
       elif can_card_be_equipped(card_to_play, next_card):
         card_to_play = equip_card(card_to_play, next_card)
-        has_valid_combination = True
       else:
-        card_to_play = result_from_failed_fusion(card_to_play, next_card)
+        has_failed_step = True
+        break
 
-    if has_valid_combination:
+    if not has_failed_step:
       results.append((fusion_combination, card_to_play))
 
   results.sort(key=lambda result: result[1].attack, reverse=True)
